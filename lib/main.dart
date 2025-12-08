912237e200dfff70a6fcd0c3cb30f557f30f73fb
moeinhe83 <moeinheshmati1383@gmail.com>
2025-12-09 02:27:55 +0330

    Edit Time

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,6 @@ class _HomePageState extends State<HomePage> {
               Text("00"),
               Text(":"),
               Text("00"),
-              Text(":"),
             ],
           ),
         ],
